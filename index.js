35c2131b20889350d3efda761bc3c15cd31c1426
oncomouse <oncomouse@gmail.com>
2018-06-26 14:30:43 -0500

    Don't preload all routes in index.html

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-module.exports = () => {
-
+module.exports = (api) => {
+  // We delete the plugins entry that preloads all the async route chunks:
+  api.configureWebpack((config) => {
+    config.plugins.forEach((plugin, i) => {
+      if (plugin.options && plugin.options.include === 'asyncChunks') {
+        config.plugins.splice(i, 1);
+      }
+    });
+  });
 };
